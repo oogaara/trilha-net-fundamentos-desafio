3590a31ac22f8c6b85966535c16a1fdfb9a6ad44
Felipe Felix <felipeestudonauta@gmail.com>
2023-12-23 13:58:17 -0300

    Minha solução

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -24,11 +24,11 @@ while (exibirMenu)
 {
     Console.Clear();
     Console.WriteLine("Digite a sua opção:");
-    Console.WriteLine("1 - Cadastrar veículo");
-    Console.WriteLine("2 - Remover veículo");
-    Console.WriteLine("3 - Listar veículos");
-    Console.WriteLine("4 - Encerrar");
-
+    Console.WriteLine("[1] \t Cadastrar veículo");
+    Console.WriteLine("[2] \t Remover veículo");
+    Console.WriteLine("[3] \t Listar veículos");
+    Console.WriteLine("[4] \t Encerrar");
+    Console.Write("> ");
     switch (Console.ReadLine())
     {
         case "1":
